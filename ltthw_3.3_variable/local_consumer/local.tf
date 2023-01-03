4437ce5e9d434fb5b293511ad6bedc75aa3f7e41
Sumit Das <sumit8das@gmail.com>
2023-01-03 13:12:56 +0530

    Section 3.3 : Variable in Modules Wrap Up

--- ltthw_3.3_variable/local_consumer/local.tf
+++ ltthw_3.3_variable/local_consumer/local.tf
@@ -2,7 +2,11 @@ locals {
   myfilename2 = "locals_filename.txt"
 }
 
+locals {
+  content = "Sumit Das - Dark Lord of DevOps"
+}
 module "hello" {
   source   = "../variable_module"
   filename = local.myfilename2
+  content = local.content
 }
--- ltthw_3.3_variable/variable_module/variable.tf
+++ ltthw_3.3_variable/variable_module/variable.tf
@@ -1,8 +1,13 @@
 variable "filename" {
-  default = "varibale_filename.txt"
+  default = "variable_filename.txt"
+}
+
+variable "content" {
+  default = "SD - Dark Lord DevOps"
 }
 
 resource "local_file" "local_file_variable" {
-  content  = "Sumit Das - Dark Lord of DevOps"
+  # content  = "Sumit Das - Dark Lord of DevOps"
   filename = var.filename
+  content = var.content
 }
